e407c301974e374256a1029190aa1775da8824d8
AbdullohKomil <akomiljonov@icloud.com>
2023-06-04 19:59:31 +0500

    comfort room end responsive end

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { SeeVilla } from './components/SeeVilla/SeeVilla';
 import { Header } from './components/Header/Header';
+import { ComfortRoom } from './components/ComfortRoom/ComfortRoom';
 
 function App() {
   return (
     <>
       <Header />
       <SeeVilla />
+      <ComfortRoom />
     </>
   );
 }
