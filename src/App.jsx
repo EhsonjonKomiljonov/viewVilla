61a92eceac2e81f4e64044968b534eec2d663ac9
EhsonjonKomiljonov <ekomiljonov29@gmail.com>
2023-06-07 09:30:59 +0500

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,15 @@ function App() {
   const [loader, setLoader] = useState(true);
   setTimeout(() => {
     setLoader(false);
-  }, 4500);
+  }, 3500);
 
   const container = {
     hidden: { opacity: 0, x: 0 },
     visible: {
-      x: [0, 0, 0, 0, 0, 700],
-      opacity: [0, 0, 0, 0, 1, 1, 1, 0],
-      transition: { duration: 3.7, delay: 1 },
+      x: [-300, 0, 0],
+      scale: [1, 1, 1, 1, 5],
+      opacity: [0, 1, 1, 1, 1, 1, 0],
+      transition: { duration: 2.7, delay: 1 },
     },
   };
 
@@ -32,20 +33,32 @@ function App() {
         <motion.img
           initial={{ x: 0, opacity: 0 }}
           animate={{
-            x: [-700, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 2, 3, 4, 700],
+            x: [-700, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0],
+            scale: [1, 1, 1, 1, 5],
             opacity: [
               1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-              1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-              1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-              1, 1, 1, 1, 1, 1, 0,
+              1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
             ],
-            transition: { duration: 4 },
+            transition: { duration: 3.9 },
           }}
           src={Loader}
           alt="Loading..."
         />
-        <motion.p variants={container} initial="hidden" animate="visible">
-          View Villa
+        <motion.p
+          className="waviy"
+          variants={container}
+          initial="hidden"
+          animate="visible"
+        >
+          <span style={{ '--i': 1 }}>V</span>
+          <span style={{ '--i': 2 }}>i</span>
+          <span style={{ '--i': 3 }}>e</span>
+          <span style={{ '--i': 4 }}>w</span>
+          <span style={{ '--i': 5 }}>V</span>
+          <span style={{ '--i': 6 }}>i</span>
+          <span style={{ '--i': 7 }}>l</span>
+          <span style={{ '--i': 8 }}>l</span>
+          <span style={{ '--i': 9 }}>a</span>
         </motion.p>
       </div>
     );
